a1c6bea1d97fa5ecb7d7199be3eba52dedf93428
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-02 20:25:08 +0200

    3 – 8. Creating a Functional Component

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function person() {
+// 	return <h2>Hola</h2>
+// }
+
+// var person = function () {}
+
+const person = () => {
+	return <p>I'm a person!</p>
+}
+
+export default person
