9e34409c7cb04ebb96be7665a221b9c264865199
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-04 15:24:08 +0200

    4 – 5. Outputting Lists

--- src/App.js
+++ src/App.js
@@ -1,67 +1,76 @@
 import React, { Component } from 'react'
 import './App.css'
-
-import UserInput from './UserInput/UserInput'
-import UserOutput from './UserOutput/UserOutput'
+import Person from './Person/Person'
 
 class App extends Component {
 	state = {
-		username: 'supermax',
+		persons: [
+			{ name: 'Max', age: 28 },
+			{ name: 'Manu', age: 29 },
+			{ name: 'Stephanie', age: 26 },
+		],
+		otherState: 'some other value',
+		showPersons: false,
 	}
 
-	usernameChangeHandler = (event) => {
-		this.setState({ username: event.target.value })
+	switchNameHandler = (newName) => {
+		// console.log('Was clicked!');
+		// DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+		this.setState({
+			persons: [
+				{ name: newName, age: 28 },
+				{ name: 'Manu', age: 29 },
+				{ name: 'Stephanie', age: 27 },
+			],
+		})
+	}
+
+	nameChangeHandler = (event) => {
+		this.setState({
+			persons: [
+				{ name: 'Max', age: 28 },
+				{ name: event.target.value, age: 29 },
+				{ name: 'Stephanie', age: 26 },
+			],
+		})
+	}
+
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons
+		this.setState({ showPersons: !doesShow })
 	}
 
 	render() {
+		const style = {
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer',
+		}
+
+		let persons = null
+
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					{this.state.persons.map((person) => {
+						return <Person name={person.name} age={person.age} />
+					})}
+				</div>
+			)
+		}
 		return (
 			<div className='App'>
-				<ol>
-					<li>Create TWO new components: UserInput and UserOutput</li>
-					<li>
-						UserInput should hold an input element, UserOutput two paragraphs
-					</li>
-					<li>
-						Output multiple UserOutput components in the App component (any
-						paragraph texts of your choice)
-					</li>
-					<li>
-						Pass a username (of your choice) to userOutput via props and display
-						it here
-					</li>
-					<li>
-						Add a state to the App component (=> the username) and pass the
-						username to the UserOutput component
-					</li>
-					<li>
-						Add a method to manipulate the state (=> an event-handler method)
-					</li>
-					<li>
-						Pass the event-handler method reference to the UserInput component
-						and bind it to the input-change event
-					</li>
-					<li>
-						Ensure that the new input entered byy the user overwrites the old
-						username passed to UserOutput
-					</li>
-					<li>
-						Add two-way-binding to your input (in UserInput) to also display the
-						starting username
-					</li>
-					<li>
-						Add styling of your choice to yorr components / elements in the
-						components - both with inline styles and stylesheets
-					</li>
-				</ol>
-				<UserInput
-					changed={this.usernameChangeHandler}
-					currentName={this.state.username}
-				/>
-				<UserOutput userName={this.state.username} />
-				<UserOutput userName={this.state.username} />
-				<UserOutput userName='Max' />
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working!</p>
+				<button style={style} onClick={this.togglePersonsHandler}>
+					Toggle Persons
+				</button>
+				{persons}
 			</div>
 		)
+		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 	}
 }
 
