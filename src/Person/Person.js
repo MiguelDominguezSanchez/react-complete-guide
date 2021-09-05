30f70e1e4b30af249146e43f5e1d19db82a4b7b0
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-05 17:53:11 +0200

    5 – 7. Introducing Styled Components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import './Person.css'
+// import './Person.css'
+const StyledDiv = styled.div`
+	.Person {
+		width: 60%;
+		margin: 16px auto;
+		border: 1px solid #eee;
+		box-shadow: 0 2px 3px #ccc;
+		padding: 16px;
+		text-align: center;
+	}
+
+	@media (min-width: 500px) {
+		.Person {
+			width: 450px;
+		}
+	}
+`
 
 // function person() {
 // 	return <h2>Hola</h2>
@@ -15,13 +32,14 @@ const person = (props) => {
 		},
 	}
 	return (
-		<div className='Person' style={style}>
+		// < div className='Person' style={style}>
+		<StyledDiv>
 			<p onClick={props.click}>
 				I'm {props.name} and I am {props.age} years old!
 			</p>
 			<p>{props.children}</p>
 			<input type='text' onChange={props.changed} value={props.name} />
-		</div>
+		</StyledDiv>
 	)
 }
 
